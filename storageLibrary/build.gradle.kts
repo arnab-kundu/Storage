73f45fecf4bd76fe3b6893abb8dd09936e10040b
Arnab Dell <arnab.kundu1990@gmail.com>
2025-01-28 04:57:38 +0530

    maven-publish

--- storageLibrary/build.gradle.kts
+++ storageLibrary/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("maven-publish")
 }
 
 apply {
@@ -36,10 +37,34 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
+    publishing {
+        // Publishes "fullRelease" build variant with "fullRelease" component created by
+        // Android Gradle plugin
+        singleVariant("release")
+    }
 }
 
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                groupId = "com.arnab.storage"
+                artifactId = "Storage"
+                version = "${project.ext["major"]}.${project.ext["minor"]}.${project.ext["build"]}"
+                pom {
+                    name.set("Android library for Storage system")
+                    description.set("Useful library to help Android developers to work with Files in Android 10 and higher version")
+                }
+                afterEvaluate {
+                    from(components["release"])
+                }
+            }
+        }
+    }
 }
